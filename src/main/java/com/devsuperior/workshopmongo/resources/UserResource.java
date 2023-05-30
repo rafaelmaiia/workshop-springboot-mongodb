1377137119fd17ffdbd16f5a9f1f297c040009b4
Rafael Maia <rafaelmaia3002@gmail.com>
2023-05-30 19:23:49 -0300

    GET find user posts

--- src/main/java/com/devsuperior/workshopmongo/resources/UserResource.java
+++ src/main/java/com/devsuperior/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devsuperior.workshopmongo.domain.Post;
 import com.devsuperior.workshopmongo.domain.User;
 import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
@@ -65,5 +66,12 @@ public class UserResource {
 		obj = userService.update(obj);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = userService.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts()); 
 	}
 }
