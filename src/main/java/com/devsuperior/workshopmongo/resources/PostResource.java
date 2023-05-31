49ab1b63649e21af9f2a18c729289a1f9bf705a2
Rafael Maia <rafaelmaia3002@gmail.com>
2023-05-31 18:39:58 -0300

    GET posts by title using @query

--- src/main/java/com/devsuperior/workshopmongo/resources/PostResource.java
+++ src/main/java/com/devsuperior/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.devsuperior.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.workshopmongo.domain.Post;
+import com.devsuperior.workshopmongo.resources.util.URL;
 import com.devsuperior.workshopmongo.services.PostService;
 
 @RestController
@@ -22,5 +26,12 @@ public class PostResource {
 		Post obj = postService.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
